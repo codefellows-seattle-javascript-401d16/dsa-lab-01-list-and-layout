e5cc9e74bce25adf59c190a43f93e9b4e7e42dab
Spencer Gietzen <Speng1516@comcast.net>
2017-06-17 00:09:09 -0700

    function code is all in place. figuring out object to object comparison so i can run tests

--- lab-spencer/lib/list.js
+++ lab-spencer/lib/list.js
@@ -8,26 +8,63 @@ function List() {
 
 list.List = List;
 
-List.prototype.reduce = function(callback, initial) {
-
-};
-
 List.prototype.push = function(val) {
-
+  this[this.length] = val;
+  this.length++;
 };
 
 List.prototype.pop = function() {
+  let result = this[this.length - 1];
+  this[this.length - 1] = undefined;
+  return result;
+};
 
+List.prototype.reduce = function(callback, initial) {
+  let i, accumulator;
+  if(initial) {
+    accumulator = initial;
+    i = 0;
+  } else {
+    accumulator = this[0];
+    i = 1;
+  }
+  for(i; i < this.length; i++) {
+    accumulator = callback(accumulator, this[i], i);
+  }
+  return accumulator;
 };
 
 List.prototype.map = function(callback) {
-
+  let result = new List();
+  for(let i = 0; i < this.length; i++) {
+    result.push(callback(this[i]));
+  }
+  return result;
 };
 
 List.prototype.filter = function(callback) {
-
+  let result = new List();
+  for(let i = 0; i < this.length; i++) {
+    if(callback(this[i]))
+      result.push(this[i]);
+  }
+  return result;
 };
 
 List.prototype.slice = function(start, end) {
-
+  let result = new List(), i;
+  if(start) {
+    i = start < 0 ? this.length - Math.abs(start) : start;
+  } else {
+    i = 0;
+  }
+  if(end) {
+    end = end < 0 ? this.length - Math.abs(start) : start;
+  } else {
+    end = this.length;
+  }
+  for(i; i < end; i++) {
+    result.push(this[i]);
+  }
+  return result;
 };
--- lab-spencer/test/list-test.js
+++ lab-spencer/test/list-test.js
@@ -7,20 +7,20 @@ describe('List Object', () => {
 
   describe('Initialization', () => {
     it('Should return an object with length === 0', () => {
-      expect(new List()).toEqual({length: 0});
+      expect(new List.List()).toEqual({length: 0});
     });
   });
 
   describe('Push', () => {
     it('Should add \'suuup\' and \'nah\' one after another to the end of the list', () => {
-      let list = new List();
+      let list = new List.List();
       list.push('suuup');
       expect(list).toEqual({0: 'suuup', length: 1});
       list.push('nah');
       expect(list).toEqual({0: 'suuup', 1: 'nah', length: 2});
     });
     it('Should push nothing, first to an empty list, then of length one and keep the List unchanged', () => {
-      let list = new List();
+      let list = new List.List();
       list.push();
       expect(list).toEqual({length: 0});
       list.push('YO');
@@ -31,7 +31,7 @@ describe('List Object', () => {
 
   describe('Pop', () => {
     it('Should remove the last item of the list and return it', () => {
-      let list = new List();
+      let list = new List.List();
       list.push('suuup');
       list.push('hey');
       list.push('no');
@@ -41,7 +41,7 @@ describe('List Object', () => {
       expect(list).toEqual({0: 'suuup', 1: 'hey', length: 2});
     });
     it('Should return undefined and keep the original list empty', () => {
-      let list = new List();
+      let list = new List.List();
       expect(list.pop()).toEqual(undefined);
       expect(list).toEqual({length: 0});
     });
@@ -49,7 +49,7 @@ describe('List Object', () => {
 
   describe('Reduce', () => {
     it('Should return 35', () => {
-      let list = new List();
+      let list = new List.List();
       list.push(21);
       list.push(4);
       list.push(10);
@@ -59,7 +59,7 @@ describe('List Object', () => {
       expect(reduced).toEqual(35);
     });
     it('Should return 50 (because initial === 15)', () => {
-      let list = new List();
+      let list = new List.List();
       list.push(21);
       list.push(4);
       list.push(10);
@@ -69,7 +69,7 @@ describe('List Object', () => {
       expect(reduced).toEqual(50);
     });
     it('Should throw \'TypeError: Reduce of empty list with no initial value\'', () => {
-      let list = new List();
+      let list = new List.List();
       expect(list).toEqual({length: 0});
       expect(() => list.reduce((accumulator, next) => accumulator + next)).toThrow(Error, 'TypeError: Reduce of empty list with no initial value');
     });
@@ -77,7 +77,7 @@ describe('List Object', () => {
 
   describe('Map', () => {
     it('Should return a new list of the same size with the string dog added to each value', () => {
-      let list = new List();
+      let list = new List.List();
       list.push('suuup');
       list.push('hey');
       list.push('no');
@@ -87,7 +87,7 @@ describe('List Object', () => {
       expect(mapped).toEqual({0: 'suuupdog', 1: 'heydog', 2: 'nodog', length: 3});
     });
     it('Should return an unchanged, empty list', () => {
-      let list = new List();
+      let list = new List.List();
       let mapped = list.map(item => item + 'dog');
       expect(mapped).toEqual({length: 0});
     });
@@ -95,7 +95,7 @@ describe('List Object', () => {
 
   describe('Filter', () => {
     it('Should return a list with only the strings from the original', () => {
-      let list = new List();
+      let list = new List.List();
       list.push('suuup');
       list.push(3);
       list.push('hey');
@@ -108,7 +108,7 @@ describe('List Object', () => {
       expect(filtered).toEqual({0: 'suuup', 1: 'hey', 2: 'no', length: 3});
     });
     it('Should return the same list', () => {
-      let list = new List();
+      let list = new List.List();
       list.push('suuup');
       list.push('hey');
       list.push('no');
@@ -118,7 +118,7 @@ describe('List Object', () => {
       expect(filtered).toEqual({0: 'suuup', 1: 'hey', 2: 'no', length: 3});
     });
     it('Should return the same empty list', () => {
-      let list = new List();
+      let list = new List.List();
       expect(list).toEqual({length: 0});
       let filtered = list.filter(item => item);
       expect(list).toEqual({length: 0});
@@ -128,7 +128,7 @@ describe('List Object', () => {
 
   describe('Slice', () => {
     it('Should return a list of 3, \'hey\', \'no\'', () => {
-      let list = new List();
+      let list = new List.List();
       list.push('suuup');
       list.push(3);
       list.push('hey');
@@ -141,7 +141,7 @@ describe('List Object', () => {
       expect(sliced).toEqual({0: 3, 1: 'hey', 2: 'no', length: 3});
     });
     it('Should return a list from the second to last to the end 22, {}', () => {
-      let list = new List();
+      let list = new List.List();
       list.push('suuup');
       list.push('hey');
       list.push('no');
@@ -153,7 +153,7 @@ describe('List Object', () => {
       expect(sliced).toEqual({0: 22, 1: {}, length: 2});
     });
     it('Should return second item to 2nd to last item', () => {
-      let list = new List();
+      let list = new List.List();
       list.push('suuup');
       list.push(3);
       list.push('hey');
@@ -166,7 +166,7 @@ describe('List Object', () => {
       expect(sliced).toEqual({0: 3, 1: 'hey', 2: 'no', 3: 22, length: 4});
     });
     it('Should return an empty list', () => {
-      let list = new List();
+      let list = new List.List();
       expect(list).toEqual({length: 0});
       let sliced = list.slice(15, -5);
       expect(list).toEqual({length: 0});
