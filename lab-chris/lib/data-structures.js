50d64a826f5e8a0780fb8d0e041ae710b4582af7
janousek77 <janousek77@gmail.com>
2017-06-18 02:22:44 -0700

    refactored

--- lab-chris/lib/data-structures.js
+++ lab-chris/lib/data-structures.js
@@ -4,6 +4,8 @@ const List = module.exports = function(){
   this.length = 0;
 };
 
+let list = new List();
+
 List.prototype.push = function(value){
   this[this.length] = value;
   this.length++;
@@ -18,7 +20,7 @@ List.prototype.pop = function(){
 };
 
 List.prototype.reduce = function(callback, initialValue){
-  let acc = (initialValue === undefined) ? this[0]: initialValue + this[0];
+  let acc = initialValue === undefined ? this[0]: initialValue + this[0];
   for (let i = 1 ; i < this.length; i++) {
     acc = callback(acc, this[i]);
   }
@@ -26,7 +28,6 @@ List.prototype.reduce = function(callback, initialValue){
 };
 
 List.prototype.map = function(callback){
-  let list = new List();
   for (var i = 0; i < this.length; i++) {
     list.push(callback(this[i]));
   }
@@ -34,7 +35,6 @@ List.prototype.map = function(callback){
 };
 
 List.prototype.filter = function(callback){
-  let list = new List();
   for (var i = 0; i < this.length; i++) {
     if (callback(this[i]))
       list.push(this[i]);
@@ -43,9 +43,8 @@ List.prototype.filter = function(callback){
 };
 
 List.prototype.slice = function(begin, end){
-  let list = new List();
-  // begin === undefined ? begin = 0 : begin;
-  // end === undefined ? end = this.length : end;
+  begin === undefined ? begin = 0 : begin;
+  end === undefined ? end = this.length : end;
   for (let i = begin; i < this[end]; i++) {
     list.push(this[i]);
   }
