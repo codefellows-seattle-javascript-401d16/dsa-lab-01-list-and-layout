e26e98fc5eb26c73d74315d4474129657b2a023a
zdiehlio <zachary.diehl@gmail.com>
2017-06-18 10:29:13 -0700

    wrote test and function for map

---
+++ test/test-list.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const expect = require('expect');
+const List = require('../lib/list.js');
+
+describe('testing list', () => {
+  it('should have a lenth of zero', () => {
+    let list = new List()
+    expect(list.length).toEqual(0);
+  });
+
+  describe('testing push', () => {
+    it('should append a value to the list', () => {
+      let list = new List();
+
+      list.push('yeeeeee yaaaaww');
+      expect(list[0]).toEqual('yeeeeee yaaaaww');
+      expect(list.length).toEqual(1);
+
+      list.push('booyeah');
+      expect(list[1]).toEqual('booyeah');
+      expect(list.length).toEqual(2);
+    });
+  });
+
+  describe('test pop', () => {
+    it('should remove a value', () => {
+      let list = new List();
+      list.push(1)
+      list.push(2)
+      list.push(3)
+
+      let result = list.pop();
+      expect(result).toEqual(3);
+      expect(list.length).toEqual(2);
+      expect(list[2]).toBe(undefined);
+
+      list = new List();
+      result = list.pop();
+      expect(result).toEqual(undefined);
+      expect(list.length).toEqual(0);
+    })
+});
+
+describ('test map', () => {
+  it('should return all values in array', () => {
+    let list = new List();
+    list.push(1)
+    list.push(2)
+    list.push(3)
+
+    let result = list.map();
+    expect(result).toEqual(1, 2, 3);
+
+    list = new List();
+    result = list.map();
+    expect(result).toBe(undefined);
+  });
+});
