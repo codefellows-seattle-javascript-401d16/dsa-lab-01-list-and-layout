df1c0cd4a041998bcab6c3477575518859ae3944
Scott <sdmccoy@gmail.com>
2017-07-12 08:30:49 -0700

    adjusted the filter method to add the truthy item to the list

--- lab5-scott/lib/array-method.js
+++ lab5-scott/lib/array-method.js
@@ -29,8 +29,6 @@ List.prototype.filter = function(callback){
   let result = new List();
   for (var i = 0; i < this.length; i++) {
     if (callback(this[i])) {
-      delete this[i];
-    } else{
       result.push(this[i]);
     }
   }
@@ -38,5 +36,5 @@ List.prototype.filter = function(callback){
 };
 
 List.prototype.reduce = function(callback){
-// need to build an accumulator that holds the callback function of adding the previous index number to the current one. Also an conditional statement in case there is nothing in the array to start with. 
+// need to build an accumulator that holds the callback function of adding the previous index number to the current one. Also an conditional statement in case there is nothing in the array to start with.
 };
