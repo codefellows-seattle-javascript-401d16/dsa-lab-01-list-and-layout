32929bd8c19440049886c746c82fe4afa91fa1bb
adan.rabadan <evilb4k4@gmail.com>
2017-08-10 15:52:16 -0700

    added some test and readme

---
+++ lab-adan/test/list-test.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const expect = require('expect');
+const List = require('../lib/list.js');
+
+describe('testing list', () => {
+  it('should have a lenth of zero', () => {
+    let list = new List();
+    expect(list.length).toEqual(0);
+  });
+
+  describe('testing push', () => {
+    it('should append a value to the list', () => {
+      let list = new List();
+
+      list.push('hello');
+      expect(list[0]).toEqual('hello');
+      expect(list.length).toEqual(1);
+
+      list.push('world');
+      expect(list[1]).toEqual('world');
+      expect(list.length).toEqual(2);
+    });
+  });
+
+  describe('test pop', () => {
+    it('should remove a value', () => {
+      let list = new List();
+      list.push(1);
+      list.push(2);
+      list.push(3);
+
+      let result = list.pop();
+      expect(result).toEqual(3);
+      expect(list.length).toEqual(2);
+      expect(list[2]).toBe(undefined);
+
+      list = new List();
+      result = list.pop();
+      expect(result).toEqual(undefined);
+      expect(list.length).toEqual(0);
+    });
+  });
+
+});
